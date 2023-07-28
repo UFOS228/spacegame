65228ad4f964338ad7d6ddae5d37cd133d6b471d
UFOS228 <86252261+UFOS228@users.noreply.github.com>
2023-07-28 19:18:12 +0300

    d

---
+++ monogame-test/Component.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using monogametest;
+
+namespace Game
+{
+	public abstract class IComponent
+	{
+		public abstract void Init(GameObject );
+		public abstract void Update();
+	}
+}
+
---
+++ monogame-test/GameObject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Game
+{
+	public class GameObject
+	{
+		public Vector2 position;
+
+	}
+}
+
