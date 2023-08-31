906aed833bcfe8c797b4e532de4def91c50daa61
UFOS2288 <75661038+UFOS2288@users.noreply.github.com>
2023-08-31 14:02:28 +0300

    DebugManager
    TODO: Add this in Game1

---
+++ monogame-test/DebugManager.cs
@@ -0,0 +1,31 @@
+﻿using monogame_test;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spacegame
+{
+    public static class DebugManager
+    {
+        public static void Draw()
+        {
+            
+
+        }
+        public static void Update()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.F10))
+            {
+                Debug.WriteLine("------------Object manifest------------");
+                for (int i = 0; i < ObjectManager.objectsOnMap.Count; i++)
+                {
+                    Debug.WriteLine("Object:" + ObjectManager.objectsOnMap[i].name + " Pos:" + ObjectManager.objectsOnMap[i].position);
+                }
+                Debug.WriteLine("---------------------------------------");
+            }
+        }
+    }
+}
